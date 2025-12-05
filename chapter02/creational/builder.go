1907e68a1267a292d2c1880f7e4cb4fe8dc30d39
Asanbek09 <samudinua@gmail.com>
2025-12-05 12:05:43 +0600

    Несколько примеров по шаблонам

---
+++ chapter02/creational/builder.go
@@ -0,0 +1,60 @@
+package creational
+
+type BuildProcess interface {
+	SetWheels() BuildProcess
+	SetSeats() BuildProcess
+	SetStructure() BuildProcess
+	GetVehicle() VehicleProduct
+}
+
+type ManufacturingDirector struct{}
+
+func (f *ManufacturingDirector) Construct() {
+	//
+}
+
+func (f *ManufacturingDirector) SetBuilder(b BuildProcess) {
+	//
+}
+
+type VehicleProduct struct {
+	Wheels int
+	Seats int
+	Structure string
+}
+
+type CarBuilder struct{}
+
+func (c *CarBuilder) SetWheels() BuildProcess {
+	return nil
+}
+
+func (c *CarBuilder) SetSeats() BuildProcess {
+	return nil
+}
+
+func (c *CarBuilder) SetStructure() BuildProcess {
+	return nil
+}
+
+func (c *CarBuilder) Build() VehicleProduct {
+	return VehicleProduct{}
+}
+
+type BikeBuilder struct {}
+
+func (b *BikeBuilder) SetWheels() BuildProcess {
+	return nil
+}
+
+func (b *BikeBuilder) SetSeats() BuildProcess {
+	return nil
+}
+
+func (b *BikeBuilder) SetStructure() BuildProcess {
+	return nil
+}
+
+func (b *BikeBuilder) Build() VehicleProduct {
+	return VehicleProduct{}
+}
